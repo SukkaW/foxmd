5d7af3a8c503b227132045b9924cbdb226976b6b
SukkaW <isukkaw@gmail.com>
2025-12-30 10:04:54 +0800

    fix: add `inert` to `htmlBooleanAttributes`

--- src/html/react-dom-boolean-proeprty.ts
+++ src/html/react-dom-boolean-proeprty.ts
@@ -1,5 +1,9 @@
-// https://github.com/facebook/react/blob/65eec428c40d542d4d5a9c1af5c3f406aecf3440/packages/react-dom-bindings/src/client/ReactDOMComponent.js#L754
+/* https://github.com/facebook/react/blob/65eec428c40d542d4d5a9c1af5c3f406aecf3440/packages/react-dom-bindings/src/client/ReactDOMComponent.js#L754 */
 export const htmlBooleanAttributes = new Set([
+  // Note: also a boolean prop, React DOM actually fallthrough as boolean prop,
+  // but with extra warning logic in the dev build
+  'inert',
+
   'allowFullScreen',
   'async',
   'autoPlay',
@@ -29,6 +33,12 @@ export const htmlBooleanAttributes = new Set([
   'autoFocus'
 ]);
 
+/**
+ * An attribute that can be used as a flag as well as with a value.
+ * When true, it should be present (set either to an empty string or its name).
+ * When false, it should be omitted.
+ * For any other value, should be present with that value.
+ */
 export const htmlOverloadedBooleanAttributes = new Set([
   'capture',
   'download'
